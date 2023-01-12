c09043dbf7e5cb99c18eea6431af8fa8432e3049
Percy1134 <75370386+Percy1134@users.noreply.github.com>
2023-01-12 18:12:30 +0100

    Update Belka.java

--- src/percy/obronsie/Belka.java
+++ src/percy/obronsie/Belka.java
@@ -1,5 +1,8 @@
 package percy.obronsie;
 import java.util.ArrayList;
+/*
+ * klasa wprowadza liste punktow do arraylist belki
+ */
 class Belka {
     int wytrzymalosc = 1;
     ArrayList<ArrayList<Integer>> punkty;
