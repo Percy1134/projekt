7598feffed1f34e389b73f306bf8e57e0cf0f158
Percy1134 <75370386+Percy1134@users.noreply.github.com>
2023-01-12 18:14:13 +0100

    Update Poziom3.java

--- src/percy/obronsie/Poziom3.java
+++ src/percy/obronsie/Poziom3.java
@@ -1,15 +1,21 @@
 package percy.obronsie;
-
+/*
+ * ustalanie max ilosci belek i pozycji kulek
+ */
 class Poziom3 extends Poziom {
     Poziom3(int dlx, int dly, int przesuniecie) {
         super();
         this.maxIloscBelek = 1;
         this.maxIloscWzBelek = 1;
         this.bron = new int[2];
-        this.kulki = new int[1][2];
+        this.kulki = new int[3][2];
         this.bron[0] = (int)((dlx-przesuniecie)/2);
         this.bron[1] = (int)(dly*0.9);
         this.kulki[0][0] = (int)((dlx-przesuniecie)/2-300);
         this.kulki[0][1] = (int)(dly*0.10);
+        this.kulki[1][0] = (int)((dlx-przesuniecie)/2);
+        this.kulki[1][1] = (int)(dly*0.10);
+        this.kulki[2][0] = (int)((dlx-przesuniecie)/2+300);
+        this.kulki[2][1] = (int)(dly*0.10);
     }
 }
