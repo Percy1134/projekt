555ef395ec39328c02365291251664c907589af6
Percy1134 <75370386+Percy1134@users.noreply.github.com>
2023-01-12 18:13:36 +0100

    Update Poziom1.java

--- src/percy/obronsie/Poziom1.java
+++ src/percy/obronsie/Poziom1.java
@@ -1,9 +1,14 @@
 package percy.obronsie;
-
+/*
+ * ustalanie max ilosci belek i pozycji kulek
+ * @param this.maxIloscBelek okresla wytrzymalosc podstawowej belki
+ * @param this.maxIloscBelek okresla wytrzymalosc wzmocnionej belki
+ * @param this.bron okresla wytrzymalosc podstawowej belki
+ */
 class Poziom1 extends Poziom {
     Poziom1(int dlx, int dly, int przesuniecie) {
         super();
-        this.maxIloscBelek = 3;
+        this.maxIloscBelek = 1;
         this.maxIloscWzBelek = 1;
         this.bron = new int[2];
         this.kulki = new int[1][2];
